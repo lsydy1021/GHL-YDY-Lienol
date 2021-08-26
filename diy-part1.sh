2a989f161157ae74eb3499303cd7f82ec3c78d18
lsydy1021 <59816384+lsydy1021@users.noreply.github.com>
2021-08-26 17:31:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,4 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i s/luci-theme-bootstrap/luci-theme-bootstrap-mod/g feeds/luci/collections/luci/Makefile
